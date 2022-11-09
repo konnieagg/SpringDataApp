d29ab03e7841dfcb12655c313d20f3308630fbe8
kaggelopoul <konnieaggelopoulou@yahoo.gr>
2022-11-09 10:00:01 +0200

    My first commit

---
+++ src/main/java/com/academy/spring/data/app/spring/SpringDataApp/SpringDataAppApplication.java
@@ -0,0 +1,13 @@
+package com.academy.spring.data.app.spring.SpringDataApp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringDataAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringDataAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/academy/spring/data/app/spring/SpringDataApp/SpringDataAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.academy.spring.data.app.spring.SpringDataApp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringDataAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
